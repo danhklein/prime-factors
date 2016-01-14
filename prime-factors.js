94f3da55b17a3c31b8bde58a8f6be8a5aa34d198
Daniel Klein <danhirschklein@gmail.com>
2016-01-14 09:58:21 -0700

    light refactoring

--- prime-factors.js
+++ prime-factors.js
@@ -1,17 +1,28 @@
 //our code
 
 
-
+//Declare function myFn and pass
+//it parameter of arg.
   function myFn(arg){
+  //declare variable num and assign it
+  //arg.
     var num = arg;
+    //Declare variable primes and 
+    //asign it empty array
       var primes = [];
+    //Start a for loop with declared
+    //variable of i that assigned value of 2
+    //at every iteration the i will increase
+    //by 1 until it hits the value of number.
 
-  for(i=2; i <= num; i++) {
-    while (num%i===0 && (num !== 1)) {
-      //Learn syntax for methods, 
-      //screwed me up forever
+  for(var i=2; i <= num; i++) {
+     //the while loop says that everytime during the loop that 
+    //the conditions of there being no remainder for 0
+    while (!(num%i) && (num !== 1)) {
+
+    
       primes.push(i)
-      num=num/i;
+      num/=i;
     } 
 }
 return primes;
